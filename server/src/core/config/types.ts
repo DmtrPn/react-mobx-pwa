1e88aab69bb279f254fa1d2889fc486f893afbe6
Dima Panov <rpsdpano@gmail.com>
2022-04-01 08:59:17 +0300

    Fix config types

--- server/src/core/config/types.ts
+++ server/src/core/config/types.ts
@@ -5,10 +5,6 @@ export enum ConfigName {
     Services = 'services',
     Log = 'log',
     Redis = 'redis',
-    Google = 'google',
-    Configuration = 'configuration',
-    Sip = 'sip',
-    Openid = 'openid',
 }
 
 export interface ServerConfig {
@@ -31,38 +27,7 @@ export interface RedisConfig extends RedisOptions {
     port: number;
 }
 
-export interface GoogleConfig {
-    clientID: string;
-    clientSecret: string;
-    callbackURL: string;
-}
-
-export interface ConfigurationConfig {
-    loggedInTimeout: number;
-}
-
-export interface SipConfig {
-    sipuri: string;
-    password: string;
-    websocket: string;
-}
-
-export interface OpenidConfig {
-    issuerBaseUrl: string;
-    issuerLogoutUrl: string;
-    clientId: string;
-    clientSecret: string;
-    scope: string;
-    loginRedirectUri: string;
-    logoutRedirectUri: string;
-    responseType: string;
-}
-
 export type ConfigType =
     ServerConfig
     | ServicesConfig
-    | RedisConfig
-    | ConfigurationConfig
-    | GoogleConfig
-    | SipConfig
-    | OpenidConfig;
+    | RedisConfig;
