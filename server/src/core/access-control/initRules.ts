4509b76843d7231238b3153c5c48b5b331ed5af5
Dima Panov <rpsdpano@gmail.com>
2022-05-08 02:00:15 +0300

    remove unused

--- server/src/core/access-control/initRules.ts
+++
@@ -1,7 +0,0 @@
-import { AccessControl } from './AccessControl';
-
-import { MovieRule } from '@catalog/access/MovieRule';
-
-const accessControl = AccessControl.getInstance();
-
-accessControl.addRule(MovieRule.entity, MovieRule);
