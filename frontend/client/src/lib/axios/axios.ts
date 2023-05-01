2b28e9aee4f24944635793609a00ce1f4b502e7c
Dima Panov <rpsdpano@gmail.com>
2023-05-01 16:51:32 +0400

    some refactor axios error

--- frontend/client/src/lib/axios/axios.ts
+++ frontend/client/src/lib/axios/axios.ts
@@ -2,10 +2,30 @@ import axios, { AxiosResponse } from 'axios';
 
 // Make base url for server and client
 /* eslint-disable @typescript-eslint/dot-notation */
+// @ts-ignore
 const BASE_URL: string = typeof window !== 'undefined' && window['BASE_URL'] ? window['BASE_URL'] : '';
 
 const instance = axios.create({
     baseURL: BASE_URL,
 });
+
+instance.defaults.headers = {
+    // @ts-ignore
+    'Cache-Control': 'cache, no-store, must-revalidate',
+    Pragma: 'no-cache',
+    Expires: '0',
+};
+
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        let errorData = error.response?.data ?? error.data ?? error;
+        errorData = errorData.data?.code ? errorData.data : errorData;
+
+        const result = new Error(errorData.message);
+        result.name = errorData.code ? errorData.code : error.name;
+        return Promise.reject(result);
+    },
+);
 
 export { instance, AxiosResponse };
